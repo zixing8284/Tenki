03ead6b096b96c32b33f11dfb5e4028dc6a8fd0a
sasasa10 <zixing8284@gmail.com>
2020-05-24 21:11:18 +0800

    rewrite fixed

--- rajio.py
+++ rajio.py
@@ -2,6 +2,9 @@ import sublime
 import sublime_plugin
 import subprocess
 
+
+# todo: add statusbar text:(playing what)
+
 TENKI_SETTING_FILE = 'Tenki.sublime-settings'
 
 
@@ -10,6 +13,7 @@ class Switching(sublime_plugin.EventListener):
     hitname_go = None
     hitfm_go = None
     radio_list = None
+    popo = None
 
     def on_activated_async(self, view):
         my_favorite = sublime.load_settings(
@@ -18,60 +22,121 @@ class Switching(sublime_plugin.EventListener):
         Switching.hitfm_go = tuple(my_favorite.values())[0]
         Switching.radio_list = sublime.load_settings(
             TENKI_SETTING_FILE).get('radio_list')
+        Switching.radio_list = sublime.load_settings(
+            TENKI_SETTING_FILE).get('radio_list')
 
 
 class ListenCommand(sublime_plugin.WindowCommand):
     init = False
     v = None
     popo = None
+    url = None
 
     def init_new_file(self):
         return self.window.new_file()
 
-    def turn_on(self, url):
-        self.popo = subprocess.Popen(
+    def turn_on(self, url, urlname):
+        # Do not display CMD window
+        # st = subprocess.STARTUPINFO
+        # st.dwFlags = subprocess.STARTF_USESHOWWINDOW
+        # st.wShowWindow = subprocess.SW_HIDE
+        # popo = subprocess.Popen("ffplay -loglevel quiet -nodisp \
+        #       http://live.xmcdn.com/live/12/64.m3u8", \
+        #       stdin=subprocess.PIPE,stdout=subprocess.PIPE, \
+        #       stderr=subprocess.PIPE,startupinfo=st)
+        self.popo = Switching.popo = subprocess.Popen(
             "ffplay -loglevel quiet \
             -nodisp " + url)
+        self.v.set_read_only(False)
+        self.v.run_command(
+            'insert',
+            {'characters': '正在播放中：' + urlname + '\n'})
+        self.v.set_read_only(True)
 
-    def turn_off(self):
-        self.popo.terminate()
+    def turn_off(self, url, urlname):
+        if not Switching.popo:
+            self.turn_on(url=url, urlname=urlname)
+        else:
+            self.v.set_read_only(False)
+            # if self.url != url:
+            if ListenCommand.url != url:
+                self.popo.terminate()
+                self.v.run_command(
+                    'insert',
+                    {'characters': '切换调频\n'})
+                self.turn_on(url=url, urlname=urlname)
+            else:
+                self.v.run_command(
+                    'insert',
+                    {'characters': '正在播放这个调频\n'})
+            self.v.set_read_only(True)
 
     def run(self, hitfm=None, hitname=None, selfie=True, inital=True):
+        # if not ListenCommand.init:
         if not self.init:
-            self.v = self.init_new_file()
+            self.v = ListenCommand.v = self.init_new_file()
             self.window.focus_view(self.v)
-            # 页面被关闭时记得重置它
-            self.init = True
+            # remember to reset this in QuitCommand
+            ListenCommand.init = True
             Switching.nigeru = self.v.id()
             # new tab name
             self.v.set_name("ST网络广播")
             # no need store new tab
             self.v.set_scratch(True)
-
+            self.v.run_command(
+                'insert',
+                {'characters': '不要关闭此页面，然后打开一个调频\n'})
+            self.v.set_read_only(True)
+        # if select Listen My favorite radio
         if selfie:
             hitfm = Switching.hitfm_go
-            print(Switching.hitfm_go)
-            print(hitfm)
             hitname = Switching.hitname_go
         try:
-            self.turn_on(hitfm)
+            self.turn_off(hitfm, hitname)
+            # self.url = hitfm
+            ListenCommand.url = hitfm
         except Exception as e:
             print(e)
-            print("something wrong here")
+            print("error here")
+            pass
 
 
 class OpenMyRadioCommand(sublime_plugin.WindowCommand):
 
+    # def run(self, hitfm=None, hitname=None, selfie=False, inital=True):
     def run(self):
-        pass
+        self.window.show_quick_panel(
+            [Switching.radio_list[index][0]
+                for index in range(len(Switching.radio_list))],
+            self.on_done,
+            sublime.KEEP_OPEN_ON_FOCUS_LOST)
+
+    def on_done(self, index):
+        if index == -1:
+            return
+        self.window.run_command(
+            'listen',
+            {'hitfm': Switching.radio_list[index][1],
+             'hitname': Switching.radio_list[index][0],
+             'selfie': False})
 
 
 class StopCommand(sublime_plugin.WindowCommand):
+
     def run(self):
+        if Switching.popo:
+            Switching.popo.terminate()
+            ListenCommand.v.set_read_only(False)
+            ListenCommand.v.run_command(
+                'insert',
+                {'characters': '停止了播放\n'})
+            ListenCommand.v.set_read_only(True)
+            Switching.popo = None
         pass
 
 
 class Quit(sublime_plugin.EventListener):
+
     def on_close(self, view):
         if view.id() == Switching.nigeru:
             StopCommand(sublime_plugin.WindowCommand).run()
